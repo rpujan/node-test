8bd72aa77b12178e72a24a185ef24b80096f7f86
Ram Pujan Singh <rpujan@insightresults.com>
2023-05-05 13:10:46 +0530

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 var _ = require("lodash");
 
+//https://mockapi.io
+//
 function square(n) { return n * n };
 
 console.log(_.map([4,8], square));
+
+fetch('https://644f8c73b61a9f0c4d2534c0.mockapi.io/users/1/tasks', {
+  method: 'GET',
+  headers: {'content-type':'application/json'},
+}).then(res => {
+  if (res.ok) {
+      return res.json();
+  }
+  // handle error
+}).then(tasks => {
+  console.log(tasks);
+}).catch(error => {
+  // handle error
+})
